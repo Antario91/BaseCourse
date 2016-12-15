fe6e7e0f35dae6c704480f3869354c1c3a6db015
Antario91 <antario91@github.com>
2016-12-15 13:18:20 +0200

    Everything is working WITH WORKING HQL in OrderRepoImpl

--- PersistenceHibernateRepositoryImpl/source/main/java/persistence/orderRepository/OrderRepoImpl.java
+++ PersistenceHibernateRepositoryImpl/source/main/java/persistence/orderRepository/OrderRepoImpl.java
@@ -27,7 +27,8 @@ public class OrderRepoImpl extends GenericRepoImpl<Long, Long> implements OrderR
     public List<Product> getAllOrdersProducts(Order order) {
         return (List<Product>) getSessionFactory().getCurrentSession()
 //                .createQuery("SELECT p FROM Product p, Order o INNER JOIN o.orderItems item WHERE o.id = :orderId AND p.id = item.productId")
-                .createQuery("SELECT p FROM Product p LEFT OUTER JOIN Order o WHERE o.id = :orderId AND p.id = o.orderItems.productId")
+                .createQuery("SELECT p FROM Product p WHERE p.id IN (SELECT item.productId FROM Order o INNER JOIN o.orderItems item WHERE o.id = :orderId)")
+//                        + "JOIN Order o JOIN o.orderItems item WITH p.id = item.productId WHERE o.id = :orderId")
                 .setParameter("orderId", order.getId())
                 .list();
     }
